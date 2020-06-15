d9c9538da0bc761298bf755c1112891858c0d852
kucingz <66920207+kucingz@users.noreply.github.com>
2020-06-15 21:54:40 +0200

    eluwina
    zmiany cards zrobilem  d: - P

--- Centrum_obsługi_kart_PO_v0_1/Card.cs
+++ Centrum_obsługi_kart_PO_v0_1/Card.cs
@@ -8,10 +8,35 @@ namespace Centrum_obsługi_kart_PO_v0_1
 {
     class Card
     {
-        private string Owner;
-        private int Balance;
-        private long Card_number;
-        private DateTime Creation_date;
-        private DateTime Expiration_date;
+        List<long> CardsList = new List<long>();
+        private int cardType;
+        string bank_name;
+        //1-credit 2-debit 3-atm
+        public long cardNumber;
+        public string Get_bankName()
+        {
+            return bank_name;
+        }
+        public int Get_cardType()
+        {
+            return cardType;
+        }
+        public long Create_cardNumber()
+        {
+            Random rng = new Random();
+        RLoop:
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0) cardNumber = rng.Next(1000, 9999);
+                else cardNumber = (cardNumber * 10000) + rng.Next(1000, 9999);
+            }
+            if (CardsList.Find(x => x == cardNumber) == cardNumber) goto RLoop;
+            else
+            {
+                CardsList.Add(cardNumber);
+                return cardNumber;
+            }
+        }
     }
 }
+
